3a4222ea4fab579445ce3fe0a3626053e4b8bb15
Eli <eli@landt.co>
2022-08-08 12:45:23 +0200

    no message

--- dist/_nuxt/static/1659707685/es/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fpixi-demo\u002F_nuxt\u002Fstatic\u002F1659707685",layout:"home",error:a,state:{enableWebglHero:b,enableWebglCardA:c,seenPreloader:c,i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fes",config:{_app:{basePath:"\u002Fpixi-demo\u002F",assetsPath:"\u002Fpixi-demo\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,false));
